d071eadd99f801b82d4dc610e9257e1a8ca7b070
Fran <fsendot@itba.edu.ar>
2023-11-14 00:19:47 -0300

    Make Flying Chair core animation

--- js/flyingChairs.js
+++ js/flyingChairs.js
@@ -2,83 +2,188 @@ import * as THREE from 'three';
 import * as Geometries from './basicGeometries.js';
 
 let circlePoints = [];
+const chairsAndDiscGroup = new THREE.Group();
+const chairsGroup = new THREE.Group();
+const flyingChairs = new THREE.Group();
+
+let discHeight;
+
+let up = new THREE.Vector3( 0, 1, 0 );
+let axis = new THREE.Vector3( );
+
+let x = 0.0;
+let xSign = 1;
+let z = 0.0;
+let zSign = 1;
+let t = 0;
+let step = 0.000001;
 
 function createFlyingChairs( chairsAmount = 10, height = 70.0 ){
     if(!Number.isInteger(chairsAmount) || chairsAmount < 1){
         chairsAmount = 5;
     }
     if(Number.isNaN(height) || height < 50.0){
-        height = 70.0;
+        discHeight = 70.0;
+    } else{
+        discHeight = height;
     }
     const mainCylinder = new THREE.CylinderGeometry(7, 7, height);
     const mainCylinderMesh = new THREE.Mesh(mainCylinder, new THREE.MeshBasicMaterial({ color: 0xff0000 }));
     mainCylinderMesh.translateY(height/2 + 35);
 
-    const topPart1 = new THREE.CylinderGeometry(50, 7, 15);
-    const topPart1Mesh = new THREE.Mesh(topPart1, new THREE.MeshBasicMaterial({ color: 0x0000ff }));
-    topPart1Mesh.translateY(height + 7.5 + 35);
-    const topPart2 = new THREE.CylinderGeometry(50, 50, 7);
-    const topPart2Mesh = new THREE.Mesh(topPart2, new THREE.MeshBasicMaterial({ color: 0x0000ff }));
-    topPart2Mesh.translateY(height + 15 + 3.5 + 35);
+    const discPart1 = new THREE.Mesh(new THREE.CylinderGeometry(50, 7, 15), new THREE.MeshPhysicalMaterial({ 
+            color: 0x049ef4,
+            ior: 1.5,
+            reflectivity: 0.5,
+            iridescenceIOR: 1.3,
+            specularIntensity: 1
+        }));
+
+    discPart1.translateY(7.5);
+    const discPart2 = new THREE.Mesh(new THREE.CylinderGeometry(50, 50, 7), new THREE.MeshPhysicalMaterial({ 
+            color: 0x049ef4,
+            ior: 1.5,
+            reflectivity: 0.5,
+            iridescenceIOR: 1.3,
+            specularIntensity: 1
+        }));
+
+    discPart2.translateY(15 + 3.5);
+    
+    const disc = new THREE.Group();
+    disc
+        .add(discPart1)
+        .add(discPart2);
+
+    const auxCylinder = new THREE.Mesh(new THREE.CylinderGeometry(12, 8, 7), new THREE.MeshPhysicalMaterial({ 
+            color: 0x049ef4,
+            ior: 1.5,
+            reflectivity: 0.5,
+            iridescenceIOR: 1.3,
+            specularIntensity: 1
+        }));
+
+    auxCylinder.translateY(height + 35);
     
     const bottomPart1 = new THREE.CylinderGeometry(12, 12, 20);
-    const bottomPart1Mesh = new THREE.Mesh(bottomPart1, new THREE.MeshBasicMaterial({ color: 0x0000ff }));
+    const bottomPart1Mesh = new THREE.Mesh(bottomPart1, new THREE.MeshPhysicalMaterial({ 
+            color: 0x049ef4,
+            ior: 1.5,
+            reflectivity: 0.5,
+            iridescenceIOR: 1.3,
+            specularIntensity: 1
+        }));
     bottomPart1Mesh.translateY(10);
     const bottomPart2 = new THREE.CylinderGeometry(9, 12, 7);
-    const bottomPart2Mesh = new THREE.Mesh(bottomPart2, new THREE.MeshBasicMaterial({ color: 0x0000ff }));
+    const bottomPart2Mesh = new THREE.Mesh(bottomPart2, new THREE.MeshPhysicalMaterial({ 
+            color: 0x049ef4,
+            ior: 1.5,
+            reflectivity: 0.5,
+            iridescenceIOR: 1.3,
+            specularIntensity: 1
+        }));
     bottomPart2Mesh.translateY(23.5);
     const bottomPart3 = new THREE.CylinderGeometry(9, 9, 5);
-    const bottomPart3Mesh = new THREE.Mesh(bottomPart3, new THREE.MeshBasicMaterial({ color: 0x0000ff }));
+    const bottomPart3Mesh = new THREE.Mesh(bottomPart3, new THREE.MeshPhysicalMaterial({ 
+            color: 0x049ef4,
+            ior: 1.5,
+            reflectivity: 0.5,
+            iridescenceIOR: 1.3,
+            specularIntensity: 1
+        }));
     bottomPart3Mesh.translateY(29.5);
     const bottomPart4 = new THREE.CylinderGeometry(8, 9, 3);
-    const bottomPart4Mesh = new THREE.Mesh(bottomPart4, new THREE.MeshBasicMaterial({ color: 0x0000ff }));
+    const bottomPart4Mesh = new THREE.Mesh(bottomPart4, new THREE.MeshPhysicalMaterial({ 
+            color: 0x049ef4,
+            ior: 1.5,
+            reflectivity: 0.5,
+            iridescenceIOR: 1.3,
+            specularIntensity: 1
+        }));
     bottomPart4Mesh.translateY(33.5);
     
     circlePoints = [];
-    const circle = new THREE.EllipseCurve(
-        0,  0,            // ax, aY
-        40, 40,           // xRadius, yRadius
-    );
-    const step = 1.0/chairsAmount;
-    const pointVec = new THREE.Vector3();
+    const step = (Math.PI * 2) / chairsAmount;
     let u = 0.0;
     let chair, cableGeometry, cableMesh;
     let cableChairs = [];
     for(let i=0; i<chairsAmount ;i++){
-        circle.getPointAt(u, pointVec);
-        pointVec.applyMatrix4(new THREE.Matrix4(
-            1, 0, 0, 0,
-            0, 0, -1, 0,
-            0, 1, 0, 0,
-            0, 0, 0, 1
-        ));
+        let pointVec = new THREE.Vector3(Math.cos(u) * 40, 0, Math.sin(u) * 40);
         circlePoints.push(pointVec);
         chair = new Geometries.createChair();
-        chair.translateY(35 - 3.5);
-        chair.translateX(pointVec.x);
-        chair.translateZ(pointVec.z);
-        chair.rotateY(2*Math.PI*u);
+        chair.translateY(-height - 20 - 3.5);
         cableGeometry = new THREE.CylinderGeometry(0.5, 0.5, height + 20);
-        cableMesh = new THREE.Mesh( cableGeometry, new THREE.MeshBasicMaterial({ color: 0xffff00 }))
-        cableMesh.translateY((height + 20.0)/2 + 35);
-        cableMesh.translateX(pointVec.x);
-        cableMesh.translateZ(pointVec.z);
-        cableChairs.push(new THREE.Group().add(chair).add(cableMesh))
+        cableMesh = new THREE.Mesh( cableGeometry, new THREE.MeshPhysicalMaterial({ 
+            color: 0x0,
+            ior: 1.5,
+            reflectivity: 0.5,
+            iridescenceIOR: 1.3,
+            specularIntensity: 1
+        }));
+        cableMesh.translateY(-(height + 20.0)/2);
+        let cableAndChair = new THREE.Group();
+        cableAndChair
+            .add(chair)
+            .add(cableMesh);
+        cableAndChair.translateY(20);
+        cableAndChair.translateX(pointVec.x);
+        cableAndChair.translateZ(pointVec.z);
+        cableChairs.push({
+            cableAndChair: cableAndChair,
+            location: pointVec
+        });
         u += step;
     }
     
-
+    chairsAndDiscGroup.clear();
+    chairsGroup.clear();
     // Group that will be added to the scene
-    const group = new THREE.Group();
-    cableChairs.forEach(cc => group.add(cc));
-    group.add(mainCylinderMesh);
-    group.add(topPart1Mesh);
-    group.add(topPart2Mesh);
-    group.add(bottomPart1Mesh);
-    group.add(bottomPart2Mesh);
-    group.add(bottomPart3Mesh);
-    group.add(bottomPart4Mesh);
-    return group;
+    flyingChairs.clear();
+    
+    cableChairs.forEach(cc => {
+        chairsGroup.add(cc.cableAndChair);
+    });
+
+    chairsAndDiscGroup
+        .add(disc)
+        .add(chairsGroup);
+    chairsAndDiscGroup.translateY(height + 35);
+
+    flyingChairs
+        .add(mainCylinderMesh)
+        .add(bottomPart1Mesh)
+        .add(bottomPart2Mesh)
+        .add(bottomPart3Mesh)
+        .add(bottomPart4Mesh)
+        .add(auxCylinder)
+        .add(chairsAndDiscGroup);
+
+    flyingChairs.position.set(-200, 0, -300);
 }
 
-export { createFlyingChairs };
+function animate(){
+
+    let randX = Math.random()/5000 * xSign;
+    let randZ = Math.random()/5000 * zSign;
+    x += randX;
+    z += randZ;
+    if(x > Math.PI/64 || x < -Math.PI/64){
+        xSign *= -1;
+    }
+    if(z > Math.PI/64 || z < -Math.PI/64){
+        zSign *= -1;
+    }
+    chairsAndDiscGroup.rotateX(randX);
+    chairsAndDiscGroup.rotateZ(randZ);
+
+
+    chairsGroup.rotateY(Math.PI * t);
+    t += step;
+
+    if( t > 0.008 || t < step){
+        step = step * -1;
+        t = t < 0 ? 0 : t;
+    }
+}
+
+export { createFlyingChairs, flyingChairs, animate };
--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ rollerCoasterCamera.position.set(200, 300, 400);
 rollerCoasterCamera.lookAt(scene.position);
 
 const genericCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 5000);
-genericCamera.position.set(300, 300, 300);
+genericCamera.position.set(-400, 200, -500);
 genericCamera.lookAt(scene.position);
 
 
@@ -25,16 +25,31 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const controls = new OrbitControls(genericCamera, renderer.domElement);
 controls.screenSpacePanning=true;
 
+const floor = new THREE.Mesh( 
+    new THREE.PlaneGeometry(10000, 10000), 
+    new THREE.MeshPhysicalMaterial({ 
+        color: 0xffff00,
+        ior: 1.5,
+        reflectivity: 0.5,
+        iridescenceIOR: 1.3,
+        specularIntensity: 1
+    })
+);
+floor.rotateX(1.5 * Math.PI);
+scene.add(floor);
+
 // Defino elementos de la escena
-const ambienLight=new THREE.AmbientLight(0x222222);
+const ambienLight=new THREE.AmbientLight(0xFFFFFF);
 scene.add(ambienLight);
 
-const light1 = new THREE.PointLight(0xFFFFAA, 1);
+const light1 = new THREE.PointLight(0xFFFFFF, 1);
 light1.position.set(300.0,300.0,150.0);
+light1.lookAt(scene.position)
 scene.add(light1);
 
-const light2 = new THREE.PointLight(0xAAFFFF, 1);
+const light2 = new THREE.PointLight(0xFFFFFF, 1);
 light2.position.set(-300.0,300.0,-150.0);
+light2.lookAt(scene.position)
 scene.add(light2);
 
 const gridHelper = new THREE.GridHelper( 100,10 );
@@ -44,13 +59,13 @@ const axesHelper = new THREE.AxesHelper( 8 );
 scene.add( axesHelper );
 
 // Roller Coaster stuff
-/*
-RollerCoaster.createTrail(true, 10);
+
+RollerCoaster.createTrail(true, 15);
 scene.add(RollerCoaster.trail);
 scene.add(RollerCoaster.carrito);
-*/
 
-scene.add(FlyingChairs.createFlyingChairs(10, 70));
+FlyingChairs.createFlyingChairs(10, 60)
+scene.add(FlyingChairs.flyingChairs);
 
 document.body.appendChild(renderer.domElement);
 
@@ -60,7 +75,8 @@ function animate() {
 	requestAnimationFrame( animate );
     
     // Every Animation should be called here
-    //RollerCoaster.animate();
+    RollerCoaster.animate();
+    FlyingChairs.animate();
 
 	renderer.render( scene, currentCamera );
 }
